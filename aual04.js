e1d45c54541e96d0f8fe9d40942011e088735c09
Cristiano Santos <66706334+cristiano-santos@users.noreply.github.com>
2023-07-12 08:31:19 -0300

    Arquivos aula 04 - Operadores Matemáticos

---
+++ aual04.js
@@ -0,0 +1,44 @@
+//operação somar
+let num01=0, num02=0, ressultSoma=0
+
+num01=4
+num02=7
+
+ressultSoma= num01 + num02
+console.log(ressultSoma)
+
+//operação subtrair
+let num03=0, num04=0, resultSubtrair=0
+
+num03=9
+num04=3
+
+resultSubtrair= num03 - num04
+console.log(resultSubtrair)
+
+//incremento / decremento
+num03++
+num04--
+console.log(num03)
+console.log(num04)
+
+//operaçao multiplicar
+let numX=0, numY=0, resultMulti=0
+
+numX= 8
+numY= 5
+
+resultMulti= numX * numY
+console.log(resultMulti)
+
+//operação divisão e resto da divisão
+let numA=0, numB=0, resultDivi=0, resultMod=0
+
+numA= 17
+numB= 2
+
+resultDivi= numA / numB
+console.log(resultDivi)
+
+resultMod= numA % numB
+console.log(resultMod)
