09e1bea7aeb855321da3870f8c4de2e0ada6d498
Cristiano Santos <cristianopxt@hotmail.com>
2023-07-18 23:22:54 -0300

    Arquivos Aula 05 - Operadores Relacionais

---
+++ aula05.js
@@ -0,0 +1,15 @@
+let num01=10
+let num02=7
+let num03=4
+
+console.log(num01 < num02)
+
+/* < menor que
+   > maior que
+   <= menor ou igual
+   >= maior ou igual
+   == igual
+   != diferente
+   ++ incremento
+   -- decremento
+*/
