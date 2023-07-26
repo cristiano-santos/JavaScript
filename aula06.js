4d79090db579e77c0df108244752faa921a9a2e3
Cristiano Santos <cristianopxt@hotmail.com>
2023-07-25 23:32:55 -0300

    Arquivos aula 06 - Operadores Lógicos

---
+++ aula06.js
@@ -0,0 +1,17 @@
+// && -> and / e
+// || -> or ou
+// ! -> not não
+
+let n1, n2, n3, n4
+n1 = 8
+n2 = 2
+n3 = 9
+n4 = 5
+
+console.log((n1>n4)||(n3<n2))
+
+if((n4 > n1) || (n3 > n2)){
+    console.log("verdadeiro")
+}else{
+    console.log("falso")
+}
